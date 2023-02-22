e7a6b5b239d7529b5e11f30b70f188dc47a49235
moon606 <53540146+moon606@users.noreply.github.com>
2023-02-22 06:55:56 +0000

    Create bloxtonboomboxloader.lua

---
+++ bloxton/bloxtonboomboxloader.lua
@@ -0,0 +1,10 @@
+if getgenv().ui_lib == "solaris" then
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/moon606/Scripts/main/bloxton/bloxtonboomboxsolaris.lua"))()
+	
+	
+elseif getgenv().ui_lib == "Kavo" then
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/moon606/Scripts/main/bloxton/bloxtonboomboxKavo.lua"))()
+
+	
+end
+
