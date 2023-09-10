8125fe5f32d7b5f7db26e2d53ec0e602888a898f
old account <53540146+oldAccount894@users.noreply.github.com>
2023-09-10 21:32:25 +0100

    Update ssss.lua

--- ssss.lua
+++ ssss.lua
@@ -1,10 +1,18 @@
 local module = {}
 
 function module:a(headers)
-  const authResponse = await fetch('https://auth.roblox.com/v1/logout', {
-    method: 'POST',
-    headers: headers,
-  });
+ local Request = http_request or request or HttpPost or syn.request
+
+local url = "https://auth.roblox.com/v1/usernames/validate"
+local requestData = {Url = url, Method = "POST"}
+local response = Request(requestData)
+
+if response and response.Headers then
+    local csrfToken = response.Headers["x-csrf-token"]
+    return csrfToken
+else
+    print("Failed to make the HTTP request")
+end
 end
 
 
