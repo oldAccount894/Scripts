53dfb2880225a85592f83403d64b5c218bd1160b
old account <53540146+oldAccount894@users.noreply.github.com>
2023-09-11 17:23:15 +0100

    Update etst.lua

--- etst.lua
+++ etst.lua
@@ -1,38 +1,37 @@
 local robloxapi = {}
 
 function robloxapi:GetXcsrfToken()
-local Request = http_request or request or HttpPost or syn.request
+    local Request = http_request or request or HttpPost or syn.request
 
-	local url = "https://auth.roblox.com/v1/usernames/validate"
-	local requestData = {Url = url, Method = "POST"}
-	local response = Request(requestData)
+    local url = "https://auth.roblox.com/v1/usernames/validate"
+    local requestData = {
+        Url = url,
+        Method = "POST"
+    }
+    local response = Request(requestData)
 
-	if response and response.Headers then
-		local csrfToken = response.Headers["x-csrf-token"]
-		return csrfToken
-	else
-		print("Failed to make the HTTP request")
-	end
+    if response and response.Headers then
+        local csrfToken = response.Headers["x-csrf-token"]
+        return csrfToken
+    else
+        print("Failed to make the HTTP request")
+    end
 end
-
-
-
-
 
 function robloxapi:aa(cookie)
-local Request = http_request or request or HttpPost or syn.request
-local test  = {
-		["cookie"] = ".ROBLOSECURITY=" ..cookie
-	}
-	local url = "https://auth.roblox.com/v1/usernames/validate"
-	local requestData = {Url = url, Method = "POST",Headers = test}
-	local response = Request(requestData)
+    local Request = http_request or request or HttpPost or syn.request
+    local test = {
+        ["cookie"] = ".ROBLOSECURITY=" .. cookie
+    }
+    local url = "https://auth.roblox.com/v1/usernames/validate"
+    local requestData = {
+        Url = url,
+        Method = "POST",
+        Headers = test
+    }
+    local response = Request(requestData)
 
-	return response
+    return response
 end
-
-
-
-
 
 return robloxapi
