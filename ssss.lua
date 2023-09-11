f04ebf82d060d5e60eb895475e873727d597dff5
old account <53540146+oldAccount894@users.noreply.github.com>
2023-09-11 15:21:06 +0100

    Update ssss.lua

--- ssss.lua
+++ ssss.lua
@@ -1,18 +1,18 @@
 local robloaapi = {}
 
-function robloxapi:GetXcsrfToken(headers)
- local Request = http_request or request or HttpPost or syn.request
+function robloxapi:GetXcsrfToken()
+local Request = http_request or request or HttpPost or syn.request
 
-local url = "https://auth.roblox.com/v1/usernames/validate"
-local requestData = {Url = url, Method = "POST"}
-local response = Request(requestData)
+	local url = "https://auth.roblox.com/v1/usernames/validate"
+	local requestData = {Url = url, Method = "POST"}
+	local response = Request(requestData)
 
-if response and response.Headers then
-    local csrfToken = response.Headers["x-csrf-token"]
-    return csrfToken
-else
-    print("Failed to make the HTTP request")
-end
+	if response and response.Headers then
+		local csrfToken = response.Headers["x-csrf-token"]
+		return csrfToken
+	else
+		print("Failed to make the HTTP request")
+	end
 end
 
 
